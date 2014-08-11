83c148188fae1cce05ae1c24489df1dafd9e9414
Tudor Laptes <tudorlaptes@gmail.com>
2014-08-11 18:18:16 -0400

    working keypad

--- src/telkpad/TelKpad.java
+++ src/telkpad/TelKpad.java
@@ -1,6 +1,5 @@
 package telkpad;
 import java.util.Scanner;
-<<<<<<< HEAD
 /**
  *
  * @author todyertuz @ plainintricacy.wordpress.com
@@ -83,52 +82,5 @@ public class TelKpad {
             q/=10;
         }
         return x;
-=======
-
-/**
- * Java code that simulates a telephone keybad. Based on this reddit daily challenge: http://goo.gl/SdX0Z4
- * @author todyerutz @ plainintricacy.wordpress.com
- */
-public class TelKpad {
-
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        System.out.println("Please enter your numbers:");
-        String nums = input.nextLine();
-        input.close();
-        
-        
-    }
-    public static boolean isValid(int x){
-            int i = x%10;
-            if(i!=1){
-            while(x!=0){
-                if(i!=x%10){
-                    return false;
-                }
-                x=x/10;
-            }
-            }
-            return true;
-        }
-    
-    public static int calcsize(int x){
-        int q=0;
-        while(x>0){
-            x/=10;
-            q++;
-        }
-        return q;
-    }
-    
-    public void splitup(String x){
-        String parts[] = x.split(" ");
-        int nums[] = new int[parts.length-1];
-        for(int i=0; i<parts.length; i++){
-            nums[i] = Integer.parseInt(parts[i]);
-        }
->>>>>>> 32e2eb4565bda8ec4e0a6939bb1961b27d25f7d8
     }
 }
